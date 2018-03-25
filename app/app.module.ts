537356cd74fb539adb8c077ea9b9b5377c415371
NetDB <netdb@NET38.hitronhub.home>
2018-03-24 21:24:35 -0600

    6

--- app/app.module.ts
+++ app/app.module.ts
@@ -16,11 +16,14 @@ import { Error404Component } from './events/erros/404-component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventListResolver } from './events/events-list-resolver.service';
 import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
     imports: [
         BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot(appRoutes)
     ],
     declarations: [
