4ed817d258600bc38a570d6be83441fbc0a62d14
NetDB <netdb@NET38.hitronhub.home>
2018-03-24 19:36:15 -0600

    5

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: 'app/user/profile.component.html'
 })
 export class ProfileComponent implements OnInit{
   profileForm: FormGroup
+  
+  constructor(private authService:AuthService, private router:Router){
+
+  }
 
   ngOnInit(){
-    let firstName = new FormControl()
-    let lastName = new FormControl()
+    let firstName = new FormControl(this.authService.currentUser.firstName)
+    let lastName = new FormControl(this.authService.currentUser.lastName)
     let profileForm = new FormGroup({
       firstName: firstName,
       lastName : lastName
     })
   }   
+
+  cancel(){
+    this.router.navigate(['events'])
+  }
 }
