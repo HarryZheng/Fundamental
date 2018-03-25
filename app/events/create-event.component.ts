82e2e904a7cd26cd0ba1c5737f41bde51e03c22a
NetDB <netdb@NET38.hitronhub.home>
2018-03-24 21:52:51 -0600

    dsf

--- app/events/create-event.component.ts
+++ app/events/create-event.component.ts
@@ -1,19 +1,24 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { EventService } from ".";
 
 @Component({
-    templateUrl: 'app/events/create-event.component.html'
+    templateUrl: 'app/events/create-event.component.html',
+    styles:[`
+    em {float:right; color:red; padding-left:10px;}
+    .error input {backgroup-color:#E3C3C5;}
+    .error ::-webkit-input-placeholder {color:#999}
+  `]
 })
 export class CreateEventComponent{
     isDirty:boolean = false
-    constructor(private router: Router){
+    constructor(private router: Router, private eventService: EventService){
     }
-
     cancel(){
         this.router.navigate(['/events'])
     }
-
-    saveEvent(){
-        console.log("event saved!")
+    saveEvent(formValues){
+        this.eventService.saveEvent(formValues)
+        this.router.navigate(['/events'])
     }
 }
