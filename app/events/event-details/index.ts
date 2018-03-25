ee4d591ad230cb347a6f404ce89476cdaac853de
hanhuizheng <hanhui.zheng@gmail.com>
2018-03-25 12:03:49 -0600

    1

--- app/events/event-details/index.ts
+++ app/events/event-details/index.ts
@@ -1,7 +1,4 @@
 export * from './event-details.component'
 export * from './event-route-activator.service'
-<<<<<<< HEAD
 export * from './session-list.component'
-=======
 export * from './create-session.component'
->>>>>>> 7675c83acf6d2478b6feb169b67fb18c65d2516c
