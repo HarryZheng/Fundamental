70793fd44390fabfb802bcd640ae1b53311d4dbe
NetDB <netdb@NET38.hitronhub.home>
2018-03-24 22:06:10 -0600

    1

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule }  from '@angular/platform-browser'
 import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 import { EventsAppComponent } from './events-app.component'
 import { EventListComponent } from './events/events-list.component'
@@ -10,13 +11,13 @@ import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { appRoutes} from './routes'
 import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './events/erros/404-component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventListResolver } from './events/events-list-resolver.service';
 import { AuthService } from './user/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { createSessionComponent } from './events';
 
 
 @NgModule({
@@ -33,6 +34,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
         NavBarComponent,
         EventDetailsComponent,
         CreateEventComponent,
+        createSessionComponent,
         Error404Component
     ],
     providers: [[
