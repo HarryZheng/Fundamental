7675c83acf6d2478b6feb169b67fb18c65d2516c
NetDB <netdb@NET38.hitronhub.home>
2018-03-25 11:32:40 -0600

    1

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,24 +1,13 @@
-
 import { NgModule } from '@angular/core'
 import { BrowserModule }  from '@angular/platform-browser'
 import { RouterModule } from '@angular/router';
-import { appRoutes } from './routes';
-
-import { EventsAppComponent } from './events-app.component'
-import { EventListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
-import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event.component';
-import { Error404Component } from './events/erros/404-component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
-import { AuthService } from './user/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { createSessionComponent } from './events';
-
+import { appRoutes } from './routes';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventsAppComponent } from './events-app.component';
+import { createSessionComponent, EventListComponent, EventThumbnailComponent, EventDetailsComponent, CreateEventComponent, Error404Component, EventService, EventRouteActivator, EventListResolver } from './events';
+import { ToastrService } from './common';
+import { AuthService } from './user';
 
 @NgModule({
     imports: [
