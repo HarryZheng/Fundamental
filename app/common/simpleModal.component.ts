efe1cc5f30e2f7c2023872b3f2ef92f5894e12e1
Harry Zheng <Harry.Zheng@live.com>
2018-03-26 23:03:20 -0600

    7

--- app/common/simpleModal.component.ts
+++ app/common/simpleModal.component.ts
@@ -12,11 +12,15 @@ import { JQUERY_TOKEN } from "./jQuery.service";
 export class SimpleModalComponent{
     @Input() title: string;
     @Input() elementId: string;
+    @Input() closeOnBodyClick: string;
     @ViewChild('modalContainer') containerEl: ElementRef;
+
 
     constructor(@Inject(JQUERY_TOKEN) private $: any) {
     }
     closeModal(){
+        if (this.closeOnBodyClick.toLocaleLowerCase()=== 'true'){
         this.$(this.containerEl.nativeElement).modal('hide');
+        }
     }
 }
